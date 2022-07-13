2c1afb15668cca26b41e812827d4b3f3aaff7735
Ziang <2787086708@qq.com>
2022-07-13 21:45:59 +0800

    完成命令行界面

---
+++ display.h
@@ -0,0 +1,11 @@
+#ifndef _DISPLAY_H
+#define _DISPLAY_H
+
+int Menu0();    // 初始菜单
+int Menu1();    // 菜单1
+int Menu1_1();  // 菜单1.1
+int Menu1_2();  // 菜单1.2
+int Menu2();    // 菜单2
+int Menu3();    // 菜单3
+
+#endif
