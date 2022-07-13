9a90c3378a4c6e4f67ced190c72ee609727a51ac
Ziang <2787086708@qq.com>
2022-07-14 04:38:10 +0800

    完善函数声明

--- vaccine.h
+++ vaccine.h
@@ -2,7 +2,6 @@
 #define _VACCINE_H
 
 #define VACCINEMAX 5000
-
 #define VACCINENUM 7  // 疫苗种数
 
 // 疫苗信息
@@ -12,22 +11,24 @@ typedef struct {
 } Vaccine;
 Vaccine vaccines[VACCINEMAX];
 
+char vaccine_type[VACCINENUM][30];
+
 // 读取 VaccinePath 中的数据到结构体，成功返回0，失败-1
 int ReadVaccine();
 
 // 将结构体写入文件，成功返回0，失败-1
 int WriteVaccine();
 
+// 查找疫苗信息
+int SearchVaccine(long long id);
+
 // 增加疫苗信息，成功返回0，失败-1
-int AddVaccine();
+int AddVaccine(Vaccine new_vaccine);
 
 // 删除疫苗信息，成功返回0，失败-1
-int DeleteVaccine();
-
-// 查找疫苗信息
-void SearchVaccine(long long id);
+int DeleteVaccine(long long id);
 
 // 修改疫苗信息
-void EditVaccine(long long id);
+void EditVaccine();
 
 #endif
